ce1d4934a53ab49eedf09abd917af7bffa75b441
Benoit BERAUD <benoit.beraud@gmail.com>
2021-04-06 22:42:25 +0200

    Dummy VPC add operation

--- vpc.go
+++ vpc.go
@@ -12,3 +12,16 @@ type Vpc struct {
 func (client Client) VpcGetList() ([]Vpc, error) {
 	return client.account.Vpcs, nil
 }
+
+func (client Client) VpcAdd() error {
+	client.account.Vpcs = append(client.account.Vpcs,
+		Vpc{
+			Id: "1234",
+		},
+	)
+	err := client.persistState()
+	if err != nil {
+		return err
+	}
+	return nil
+}

c774237bd00f859f60fc6d2dda4db945e04beb81
Benoit BERAUD <benoit.beraud@gmail.com>
2021-04-12 22:43:10 +0200

    Add support to localFileLocation which does not exists yet, including parent paths

--- client.go
+++ client.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"math/rand"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -13,10 +15,14 @@ type Client struct {
 	doNotPersist      bool
 }
 
-func OpenClientFromLocalStorage(localFileLocation string) (client *Client, err error) {
+func OpenClientFromLocalStorage(localFileLocation string) (*Client, error) {
 	rand.Seed(time.Now().Unix())
-	client = &Client{}
+	client := &Client{}
 	client.localFileLocation = localFileLocation
+	_, err := os.Stat(client.localFileLocation)
+	if os.IsNotExist(err) {
+		return client, nil
+	}
 	fileContent, err := ioutil.ReadFile(client.localFileLocation)
 	if err != nil {
 		return nil, err
@@ -32,6 +38,10 @@ func OpenClientFromLocalStorage(localFileLocation string) (client *Client, err e
 func (client *Client) persistState() error {
 	if client.doNotPersist {
 		return nil
+	}
+	path := filepath.Dir(client.localFileLocation)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.MkdirAll(path, os.ModePerm)
 	}
 	b, err := json.MarshalIndent(client.account, "", "\t")
 	if err != nil {
